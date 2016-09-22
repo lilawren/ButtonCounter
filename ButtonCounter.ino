6c7f0cab9e51abaee7bddd4e239c50661c2b9033
lali <li.lawrly@gmail.com>
2016-09-21 23:32:41 -0700

    Added source file

---
+++ ButtonCounter.ino
@@ -0,0 +1,288 @@
+/*  Button Counter Version 1.1
+ *  
+ *  Author: LALI 
+ *  Last updated: 6/23/2015
+ *  Details: Displays number of button presses on a 4-digit 7 segment display and remembers the count in EEPROM.
+ */
+
+
+#include "EEPROM.h"
+
+//set the cathode interface
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+int p = 8;
+//set the anode interface
+int d4 = 9;
+int d3 = 10;
+int d2 = 11;
+int d1 = 12;
+//button
+int buttonPin = 13;
+//variables
+int count = 0;
+int buttonState = 0;
+int msecs = 5;  //refresh rate
+long time = 0;
+
+void setup()
+{
+  pinMode(d1, OUTPUT);
+  pinMode(d2, OUTPUT);
+  pinMode(d3, OUTPUT);
+  pinMode(d4, OUTPUT);
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(p, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  //to reset counter to 0, uncomment following line
+  //EEPROM.update(0, count); 
+  count = EEPROM.read(0);
+}//setup()
+ 
+void loop()
+{  
+  /*  DISPLAYING COUNT  */
+  digitalWrite(d1, HIGH);
+  digitalWrite(d2, HIGH);
+  digitalWrite(d3, HIGH);
+  digitalWrite(d4, HIGH);
+
+  //1's place
+  openPort(4);
+  pickNumber(count%10);
+  delay(msecs);
+  digitalWrite(d4, HIGH);
+
+  //10's place
+  openPort(3);
+  pickNumber((count/10)%10);
+  delay(msecs);
+  digitalWrite(d3, HIGH);
+
+  //100's place
+  openPort(2);
+  pickNumber((count/100)%10);
+  delay(msecs);
+  digitalWrite(d2, HIGH);
+
+  //1000's place
+  openPort(1);
+  pickNumber(count/1000);
+  delay(msecs);
+  digitalWrite(d1, HIGH);
+
+  
+  /*  BUTTON LOGIC  */
+  buttonState = digitalRead(buttonPin);
+
+  time += 5;
+  if(time%35 == 0){
+    if(buttonState == HIGH){
+      count++;
+      EEPROM.update(0, count);
+    }//increment count
+  }//every 35msec check if button has been pressed
+  
+  if(time > 2000000000){
+    time = 0;
+  }//reset time if near the max value   
+
+}//loop()
+ 
+void openPort(int x)
+{
+  switch(x)
+  {
+  case 1: 
+    digitalWrite(d1, LOW); 
+    break;
+  case 2: 
+    digitalWrite(d2, LOW); 
+    break;
+  case 3: 
+    digitalWrite(d3, LOW); 
+    break;
+  default: 
+    digitalWrite(d4, LOW); 
+    break;
+  }
+}//enable digit place x to be written
+ 
+void pickNumber(int x)
+{
+  switch(x)
+  {
+  default: 
+    zero(); 
+    break;
+  case 1: 
+    one(); 
+    break;
+  case 2: 
+    two(); 
+    break;
+  case 3: 
+    three(); 
+    break;
+  case 4: 
+    four(); 
+    break;
+  case 5: 
+    five(); 
+    break;
+  case 6: 
+    six(); 
+    break;
+  case 7: 
+    seven(); 
+    break;
+  case 8: 
+    eight(); 
+    break;
+  case 9: 
+    nine(); 
+    break;
+  }
+}//displays number x
+ 
+/*void dispDec(int x)  //set to start the decimal point
+{
+  digitalWrite(p, HIGH);
+}*/
+ 
+void clearLEDs()  //clear contents
+{
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  digitalWrite(p, LOW);
+}
+
+
+
+//DISPLAY NUMBERS 
+void zero()  //define 0 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, LOW);
+}
+ 
+void one()  // define 1 as cathode pin switch
+{
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+}
+ 
+void two()  // define 2 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, LOW);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, LOW);
+  digitalWrite(g, HIGH);
+}
+ 
+void three()  // define 3 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, HIGH);
+}
+ 
+void four()  // define 4 as cathode pin switch
+{
+  digitalWrite(a, LOW);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void five()  // define 5 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void six()  // define 6 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void seven()  // define 7 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+}
+ 
+void eight()  // define 8 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+ 
+void nine()  // define 9 as cathode pin switch
+{
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, LOW);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+}
+
